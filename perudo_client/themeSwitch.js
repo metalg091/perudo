ca8e4f0cd19fa52671c53c805d9176d71ded69d3
metalg091 <38428187+metalg091@users.noreply.github.com>
2021-05-25 13:48:10 +0200

    fixed syntax

--- perudo_client/themeSwitch.js
+++ perudo_client/themeSwitch.js
@@ -25,7 +25,7 @@ function themeSwitch(){
 }
 function test(i){
   if(i == 0){
-      document.documentElement.style.setProperty('--mainbgcolor', 'black');
+    document.documentElement.style.setProperty('--mainbgcolor', 'black');
     document.documentElement.style.setProperty('--textcolor', 'white');
     document.documentElement.style.setProperty('--inputbgcolor', 'white');
     document.documentElement.style.setProperty('--inputbgcolorfocus', 'lightgray');
@@ -33,11 +33,11 @@ function test(i){
     document.documentElement.style.setProperty('--eventbgcolor', '#222222');
   }
   else{
-      document.documentElement.style.setProperty('--mainbgcolor', 'white');
-      document.documentElement.style.setProperty('--textcolor', 'black');
-      document.documentElement.style.setProperty('--inputbgcolor', 'lightgray');
-      document.documentElement.style.setProperty('--inputbgcolorfocus', 'gray');
-      document.documentElement.style.setProperty('--submitbghover', 'rgb(179, 179, 179)');
-      document.documentElement.style.setProperty('--eventbgcolor', '#F4F4F4');
+    document.documentElement.style.setProperty('--mainbgcolor', 'white');
+    document.documentElement.style.setProperty('--textcolor', 'black');
+    document.documentElement.style.setProperty('--inputbgcolor', 'lightgray');
+    document.documentElement.style.setProperty('--inputbgcolorfocus', 'gray');
+    document.documentElement.style.setProperty('--submitbghover', 'rgb(179, 179, 179)');
+    document.documentElement.style.setProperty('--eventbgcolor', '#F4F4F4');
   }
 }
