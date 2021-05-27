2f52ff51e6a241c40b0d55da21e51bf8c14ad3b3
metalg091 <tibi.furesz@gmail.com>
2021-05-27 13:40:49 +0200

    customtheme cookie setup

---
+++ perudo_client/customThemeSaver.php
@@ -0,0 +1,22 @@
+<?php
+  $bgc = $_GET["bgc"];
+  $txtc = $_GET["txtc"];
+  $inbgc = $_GET["inbgc"];
+  $inbgcfocus = $_GET["inbgcfocus"];
+  $buttonbgchover = $_GET["$buttonbgchover"];
+  setcookie(bgc, $bgc);
+  setcookie(txtc, $txtc);
+  setcookie(inbgc, $inbgc);
+  setcookie(inbgcfocus, $inbgcfocus);
+  setcookie(buttonbgchover, $buttonbgchover);
+?>
+<html>
+  <head>
+    
+  </head>
+  <body>
+    <script>
+      
+    </script>
+  </body>
+</html>
