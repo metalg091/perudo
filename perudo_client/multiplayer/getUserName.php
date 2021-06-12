bf903d2ef263784d4ca77e8d4f7d060c75086b2c
metalg091 <tibi.furesz@gmail.com>
2021-06-13 00:49:20 +0200

    Migrated to SQLite

--- perudo_client/multiplayer/getUserName.php
+++ perudo_client/multiplayer/getUserName.php
@@ -12,43 +12,35 @@ $_SESSION["username"] = $_GET["username"];
 </head>
 <body>
     <?php 
-        $servername = "localhost";
-        $username = "root";
-        $password = "";
-        $dbname = "perudo";
-        $conn = mysqli_connect($servername, $username, $password, $dbname);
-        if (!$conn){
-            die("Connection failed: " . mysqli_connect_error());
-        }
-        $sql = "SELECT playersInGame FROM game WHERE id = 0";
-	    $result = mysqli_query($conn, $sql);
-        if (mysqli_num_rows($result) > 0) {
-            $row = mysqli_fetch_assoc($result);
-            $into = $row["playersInGame"] + 1;
-            echo $into;
-            $_SESSION["id"] = $into;
-            $sql = "UPDATE game SET name = '" . $_GET["username"] . "' WHERE id = " . $into;
-            if (mysqli_query($conn, $sql)) {
-                echo "user registered succesfully";
-                $sql = "UPDATE game SET playersInGame = " . $into . " WHERE id = 0";
-                if (mysqli_query($conn, $sql)){
-                    echo "done";
-                    $whatnow = 0;
-                }
-                else{
-                    $whatnow = 1;
-                }
-            }
-            else{
-                echo "coudn't record player";
-                $whatnow = 1;
-            }
-        }
-        else {
-            echo "no result/operational error";
-            $whatnow = 1;
-        }
-        mysqli_close($conn);
+        $db = new SQLite3('../databases/perudo.sqlite', SQLITE3_OPEN_CREATE | SQLITE3_OPEN_READWRITE);
+        $db->query('CREATE TABLE IF NOT EXISTS "game" (
+            "id" INTEGER UNIQUE PRIMARY KEY AUTOINCREMENT NOT NULL,
+            "name" TEXT,
+            "cubes" INTEGER DEFAULT "5",
+            "numbers" INTEGER DEFAULT "12345",
+            "cPlayerId" INTEGER DEFAULT null,
+            "playersInGame" INTEGER DEFAULT null,
+            "cycle" INTEGER DEFAULT null)');
+
+        $db->exec('BEGIN');
+        $db->query('INSERT OR IGNORE INTO "game" ("id", "name", "cubes", "numbers", "cPlayerId", "playersInGame", "cycle") VALUES ("0", "system", "null", "null", "0", "0", "0")');
+        $db->exec('COMMIT');
+
+        $db->query('CREATE TABLE IF NOT EXISTS "eventtable" (
+            "orders" INTEGER UNIQUE PRIMARY KEY AUTOINCREMENT NOT NULL,
+            "ide" INTEGER,
+            "guess" TEXT,
+            "who" INTEGER DEFAULT "0"
+            )');
+        
+        $db->exec('BEGIN');
+        $db->query('INSERT INTO "game" ("name") VALUES ("' . $_GET["username"] . '")');
+        $db->query('UPDATE "game" SET playersInGame = playersInGame + 1 WHERE id = 0');
+        $db->exec('COMMIT');
+        
+        $_SESSION["id"] = $db->querySingle('SELECT playersInGame FROM "game" WHERE id = 0');
+        echo $_SESSION["id"];
+        $db->close();
     ?>
     <h2>Your name</h2>
     <h3 id="username">
@@ -65,13 +57,13 @@ $_SESSION["username"] = $_GET["username"];
         <?php
             echo "themeSetup(" . $_COOKIE["theme"] . ");";
         ?>
-        var reload = '<?php echo $whatnow; ?>';
-        if(reload == 0){
-            location.href = 'waitingForTurn.php';
-        }
+        //var reload = '<?php //echo $whatnow; ?>';
+        //if(reload == 0){
+            //location.href = 'waitingForTurn.php';
+        /*}
         else {
             setTimeout(function(){location.reload()}, 15000);
-        }
+        }*/
     </script>
 </body>
 </html>
