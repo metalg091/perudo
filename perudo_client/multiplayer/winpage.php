91067215f9d37f02e163c10e95e8b27af68fee22
metalg091 <tibi.furesz@gmail.com>
2021-06-22 21:56:25 +0200

    Redirect to waiting page if game is still on going

--- perudo_client/multiplayer/winpage.php
+++ perudo_client/multiplayer/winpage.php
@@ -14,9 +14,19 @@
 <div id="winner">
 <?php 
     $db = new SQLite3('../databases/perudo.sqlite', SQLITE3_OPEN_CREATE | SQLITE3_OPEN_READWRITE);
+    $cycle = $db->querySingle('SELECT cycle FROM "game" WHERE id = 0');
+    if($cycle != 2){
+        header('Location: waitingForTurn.php');
+        die('Game is still ongoing!!!');
+    }
     $cpi = $db->querySingle('SELECT cPlayerId FROM "game" WHERE id = 0');
     $winner = $db->querySingle('SELECT name FROM "game" WHERE id = ' . $cpi);
     echo $winner . " is the winner";
+    
+    /*$db->exec('BEGIN'); <- how to do dis?
+    $db->query('DROP TABLE "game"');
+    $db->query('DROP TABLE "eventtable"');
+    $db->exec('COMMIT');*/
 ?>
 </div>
 </body>
