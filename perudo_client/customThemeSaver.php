d6de000f58c026532ba5fd04b261f78c50dee663
metalg091 <tibi.furesz@gmail.com>
2021-06-28 14:02:30 +0200

    Improvements for custom theme
    Now it can save your theme in a database (no way to access yet tho...)

--- perudo_client/customThemeSaver.php
+++ perudo_client/customThemeSaver.php
@@ -11,10 +11,30 @@
             setcookie("inbgcfocus", $_GET["inbgcfocus"], time() + 86400 * 30, "/");
             setcookie("buttonbgchover", $_GET["buttonbgchover"], time() + 86400 * 30, "/");
             setcookie("eventbgc", $_GET["eventbgc"], time() + 86400 * 30, "/");
-            setcookie("theme", "3", time()+ 86400 * 30, "/")
+            setcookie("theme", "3", time()+ 86400 * 30, "/");
+            if(isset($_GET["upload"]) && $_GET["upload"] == "on"){
+                $db = new SQLite3('databases/theme.sqlite', SQLITE3_OPEN_CREATE | SQLITE3_OPEN_READWRITE);
+                $db->query('CREATE TABLE IF NOT EXISTS "theme" (
+                    "id" INTEGER UNIQUE PRIMARY KEY AUTOINCREMENT NOT NULL,
+                    "name" TEXT UNIQUE NOT NULL,
+                    "bgc" TEXT,
+                    "txtc" TEXT,
+                    "inbgc" TEXT,
+                    "inbgcfocus" TEXT,
+                    "buttonbghover" TEXT,
+                    "eventbgc" TEXT)');
+                try{
+                    $db->enableExceptions(true);
+                    $db->exec('BEGIN');
+                    $db->query('INSERT INTO theme ("name", "bgc", "txtc", "inbgc", "inbgcfocus", "buttonbghover", "eventbgc") VALUES ("' . $_GET["themename"] . '", "' . $_GET["bgc"] . '", "' . $_GET["txtc"] . '", "' . $_GET["inbgc"] . '", "' . $_GET["inbgcfocus"] . '", "' . $_GET["buttonbgchover"] . '", "' . $_GET["eventbgc"] . '")');
+                    $db->exec('COMMIT');
+                    header('Location: index.php');
+                }
+                catch(Exception $e){
+                    echo "theme name is already occupied!!!";
+                    echo '<br><a href="themeSetup.html">Retry</a>';
+                }
+            }
         ?>
-        <script>
-            location.href = "index.php";
-        </script>
     </body>
 </html>
--- perudo_client/index.php
+++ perudo_client/index.php
@@ -5,7 +5,6 @@ setcookie("width", "1", time() + 86400, "/");
 <html>
 <head>
     <link id="theme" rel="stylesheet" href="../main_theme.css">
-    <link rel="stylesheet" href="button.css">
     <script src="themeSwitch.js"></script>
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <style>
@@ -24,6 +23,9 @@ setcookie("width", "1", time() + 86400, "/");
         <option id="light" value="2">Light theme</option>
         <option id="custom" value="3">custom theme</option>
     </select>
+    <div style="position: relative; left: 10em; top: 1.05em; width: 25%;">
+    <button id="customize" onclick="redirect()" style="display: none; padding: 4px; border-radius: 7.5px">Change</button>
+    </div>
     <h2>Type in your username!</h2>
     
     <form id="form" action="multiplayer/getUserName.php" method="get">
@@ -45,13 +47,19 @@ setcookie("width", "1", time() + 86400, "/");
             document.getElementById("custom").selected = "selected";
             break;
     }
-
+    function redirect(){
+        location.href = 'customize.php';
+    }
     function ThemeSelector(){
         var themeId = document.getElementById("themeSelect").value;
         if(themeId == 3){
             if(getCookie("bgc") == ""){
                 location.href = 'themeSetup.html';
             }
+            document.getElementById("customize").style = "display: inline-block; padding: 4px; border-radius: 7.5px";
+        }
+        else{
+            document.getElementById("customize").style = "display: none; padding: 4px; border-radius: 7.5px";
         }
         document.cookie = "theme=" + themeId + "; expires=86400000; path=/";
         themeSetup(themeId);
