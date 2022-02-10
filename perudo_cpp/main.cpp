34190d5873827e47d59dfe19c487007e30059195
metalg091 <tibi.furesz@gmail.com>
2022-02-10 23:22:23 +0000

    refactor with vectors

--- perudo_cpp/main.cpp
+++ perudo_cpp/main.cpp
@@ -1,6 +1,6 @@
 #include <iostream>
 #include <string>
-#include <list>
+#include <vector>
 using namespace std;
 
 class playertype {
@@ -9,7 +9,7 @@ class playertype {
         string name;
         int cube = 5;
         bool HasCube = true;
-        int nums[5];
+        vector<int> nums = {0, 0, 0, 0, 0};
 };
 
 int main() {
@@ -17,40 +17,30 @@ int main() {
     int y;
     int currentPlayerId = 0;
     int guess = 10;
-    int allnumbers[6] = {0, 0, 0, 0, 0, 0};
+    int allnumber[6] = {0, 0, 0, 0, 0, 0};
     cout << "Welcome to perudo c++! \n";
     cout << "Enter your user name \n";
     cin >> x;
     cout << "How many bots?\n";
     cin >> y;
     //setup
-    list<playertype> player;
-    list<playertype>::iterator it;
-    playertype playerobj[y+1];
-    playerobj[0].name = x;
-    for (int i = 0; i < y+1; i++){
-        playerobj[i].id = i;
-        player.push_back(playerobj[i]);
-    }
-    //delete[] playerobj;
-    x.clear();
-    it = player.begin();
-    cout << it->id;
-    it++;
-    cout << it->id;
-    for (int i = 1; it != player.end(); ++i) {
-        it->name = "ai" + i;
-        ++it;
-    }
-    for (it = player.begin(); it != player.end(); ++it) {
-        cout << it->name;
-        cout << "\n";
+    vector<playertype> players;
+    playertype temp;
+    temp.id = 0;
+    temp.name = x;
+    players.push_back(temp);
+    for(int i = 1; i < y; ++i){
+        temp.id = i;
+        temp.name = "ai" + i;
+        players.push_back(temp);
     }
     //Game begin
     //roll(&player, &allnumbers);
     return 0;
 }
-//pointers......
-//void roll(playertype* player[], int *allnumbers[6]){
 
-//}
+void roll(vector<playertype> players, int* allnumber){
+    for(int x = 0; x < size(players); ++x){
+        //*allnumber[0] = 2;
+    }
+}
