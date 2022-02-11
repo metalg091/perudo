0fb0f09b76acf742d10d2978b636eaf7ff189b7e
metalg091 <tibi.furesz@gmail.com>
2022-02-11 23:00:57 +0000

    bare frame of game

--- perudo_cpp/main.cpp
+++ perudo_cpp/main.cpp
@@ -11,8 +11,14 @@ class playertype {
         int id;
         string name;
         int cube = 5;
-        bool HasCube = true;
+        bool hasCube = true;
+        bool isAi;
         vector<int> nums = {0, 0, 0, 0, 0};
+        playertype(int id_n, string name_n, bool isAi_n = true){
+            id = id_n;
+            name = name_n;
+            isAi = isAi_n;
+        }
 };
 
 void roll(vector<playertype>& players, int* allnumber){
@@ -25,13 +31,42 @@ void roll(vector<playertype>& players, int* allnumber){
     }
 }
 
+bool isThisBigger(int inp, int last){
+    if((to_string(last)[-1] == '1') != (to_string(inp)[-1] == '1')){
+        if(to_string(last)[-1] == '1'){
+            last = last * 2;
+        } else{
+            last = last / 2;
+        }
+        return (inp > last);
+    } else{
+        return (inp > last);
+    }
+}
+
+int getGuess(int lastOne){
+    int current;
+    cout << "Enter your guess!\n";
+    cin >> current;
+    cout << "\n";
+    if(isThisBigger(current, lastOne)){
+        return current;
+    } else{
+        cout << "Guess must be bigger than " << lastOne << "\n";
+        getGuess(lastOne);
+    }
+    return -1;
+}
+
 int main() {
     time_t nTime;
-    srand((unsigned) time(&nTime)); //to make random numbers really number 
+    srand((unsigned) time(&nTime)); //to make random numbers really random 
+    bool game = true;
     string x;
     int y;
     int currentPlayerId = 0;
     int guess = 10;
+    int prevGuess;
     int allnumber[6] = {0, 0, 0, 0, 0, 0};
     cout << "Welcome to perudo c++! \n";
     cout << "Enter your user name \n";
@@ -40,10 +75,9 @@ int main() {
     cin >> y;
     //setup
     vector<playertype> players;
-    playertype temp;
-    temp.id = 0;
-    temp.name = x;
+    playertype temp(0, x, false);
     players.push_back(temp);
+    temp.isAi = false;
     for(int i = 1; i <= y; ++i){
         temp.id = i;
         temp.name = "ai" + i;
@@ -51,7 +85,19 @@ int main() {
     }
     roll(players, allnumber);
     //Game begin
-    
+    while (game){
+        for(int i = 0; i < size(players); ++i){
+            prevGuess = guess;
+            if(i == 0){
+                guess = getGuess(guess);
+                if (guess == -1){
+                    cout << "error";
+                }
+            } else{
+                //ai()
+            }
+        }
+    }
     /*for (int y = 0; y < size(players); ++y){
         cout << "player " << y << "cubes are: ";
         for (int x = 0; x < 6; ++x){
