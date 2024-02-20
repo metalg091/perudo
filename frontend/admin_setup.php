1dfeab2c9a440fa7ab9bd9f35e57382fb47d26c6
Fürész Tibor Teodor <jaoycb@inf.elte.hu>
2024-02-20 23:22:35 +0100

    Cleared Trash
    Removed unnecessary files

--- frontend/admin_setup.php
+++
@@ -1,9 +0,0 @@
-<html>
-    <?php
-        $db = new SQLite3('databases/hub.sqlite', SQLITE3_OPEN_CREATE | SQLITE3_OPEN_READWRITE);
-        $db->query('CREATE TABLE IF NOT EXISTS "hub" (
-            "id" INTEGER UNIQUE NOT NULL,
-            "isdisplay" INTEGER,
-            "date" REAL)');
-    ?>
-</html>
