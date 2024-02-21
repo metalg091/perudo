b0596b749d81feca457702f7a118d2863da45dba
Fürész Tibor Teodor <jaoycb@inf.elte.hu>
2024-02-21 11:22:59 +0100

    Working post!
    Got to a point where I can send a username to the server and recieve it back!!!!

--- backend/app/Main.hs
+++ backend/app/Main.hs
@@ -1,14 +1,29 @@
 {-# LANGUAGE OverloadedStrings #-}
 import Web.Scotty
+import Data.Aeson (FromJSON, ToJSON)
+import GHC.Generics (Generic)
+import Network.Wai.Middleware.Cors (simpleCors)
+
+newtype User = User { userName :: String } deriving (Show, Generic)
+instance ToJSON User
+instance FromJSON User
 
 main :: IO()
 main = do
     scotty 3000 routes
 
+
 routes :: ScottyM()
 routes = do
+    middleware simpleCors
     get "/" $ genHelloWord
+    post "/join" $ enlistUser
 
 genHelloWord :: ActionM()
 genHelloWord = do
-     text "Hello, World!"
+    text ""
+
+enlistUser :: ActionM()
+enlistUser = do
+    user <- jsonData :: ActionM User
+    json user
