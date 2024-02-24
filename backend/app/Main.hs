1ab1aefbfd1255dae78e038b31e5769474d31fdc
Fürész Tibor Teodor <jaoycb@inf.elte.hu>
2024-02-24 18:16:33 +0100

    Working DB
    Got to a point where the database is working!!! BUT I can't get the browser to save any cookies :/

--- backend/app/Main.hs
+++ backend/app/Main.hs
@@ -8,13 +8,21 @@ import Database.SQLite.Simple
 import Database.SQLite.Simple.FromRow
 import Text.StringRandom
 import Data.Text (Text)
+import Data.Text.Lazy as LT
 import Foreign.Marshal.Unsafe (unsafeLocalState)
+import GHC.TypeError (ErrorMessage(Text))
 
 data User = User { userName :: String, gameId :: Int } deriving (Show, Generic)
 instance ToJSON User
 instance FromJSON User
 
 data Session = Session String String Int String deriving (Show)
+
+data GetGame = GetGame Int Int deriving (Show)
+instance FromRow GetGame where
+    fromRow = GetGame <$> field <*> field
+instance ToRow GetGame where
+    toRow (GetGame id_ gameid) = toRow (id_, gameid)
 
 --instance FromRow TestField where
 --    fromRow = TestField <$> field <*> field
@@ -28,9 +36,11 @@ main = do
     -- connect to the database in memory
     conn <- open ":memory:"
     -- create session table (id :: INT, sesid :: TEXT, name :: TEXT, gameid :: INT, numbers :: TEXT)
-    execute_ conn "CREATE TABLE session (id INTEGER PRIMARY KEY, sesid TEXT, name TEXT, gameid INT, numbers TEXT)"
+    execute_ conn "CREATE TABLE IF NOT EXISTS session (id INTEGER PRIMARY KEY, sesid TEXT, name TEXT, gameid INT, numbers TEXT)"
+    --execute_ conn "CREATE TABLE session (id INTEGER PRIMARY KEY, sesid TEXT, name TEXT, gameid INT, numbers TEXT)"
     -- create game table (id :: INT, gameid :: INT, participants :: INT, ongoing :: BOOL, host :: TEXT)
-    execute_ conn "CREATE TABLE game (id INTEGER PRIMARY KEY, gameid INT UNIQUE, participants INT, ongoing BOOL, host TEXT)"
+    execute_ conn "CREATE TABLE IF NOT EXISTS game (id INTEGER PRIMARY KEY, gameid INT UNIQUE, participants INT, ongoing BOOL, host TEXT)"
+    --execute_ conn "CREATE TABLE game (id INTEGER PRIMARY KEY, gameid INT UNIQUE, participants INT, ongoing BOOL, host TEXT)"
     -- start web server
     scotty 3000 $ routes conn
     -- close the connection
@@ -51,25 +61,30 @@ genHelloWord = do
 enlistUser :: Connection -> ActionM()
 enlistUser conn = do
     user <- jsonData :: ActionM User
-    setSimpleCookie "SESSIONID" $ unsafeLocalState $ initUser conn user
-    json $ user
+    sessionId <- liftIO $ initUser conn user
+    let cookie = "SESSIONID=\"" <> sessionId <> "\"; Path=/; SameSite=Lax"
+    setHeader "Set-Cookie" (LT.fromStrict cookie)
+    --setSimpleCookie "SESSIONID" sessionId
+    json user
 
 -- can't get this right
-initUser :: Connection -> User -> IO Text
+initUser :: Connection -> User -> IO Data.Text.Text
 initUser conn (User name 0) = do
-    sesid <- stringRandomIO "????????????????"
-    gameid <- getFreeId conn
-    execute conn "INSERT INTO session (sesid, name, gameid, numbers) VALUES (?, ?, ?, ?)" (sesid, name, gameid, "00000")
-    execute conn "INSERT INTO game (gameid, participants, ongoing) VALUES (?, ?, ?)" (getFreeId conn, 1, False)
+    sesid <- stringRandomIO "................"
+    gameId <- getFreeId conn
+    execute conn "INSERT INTO session (sesid, name, gameid, numbers) VALUES (?, ?, ?, ?)" (sesid :: Data.Text.Text, name :: String, gameId :: Int, "00000" :: String)
+    execute conn "INSERT INTO game (gameid, participants, ongoing) VALUES (?, ?, ?)" (gameId :: Int, 1 :: Int, False :: Bool)
     return sesid
-initUser conn (User name gameid) = do
-    sesid <- stringRandomIO "????????????????"
-    execute conn "INSERT INTO session (sesid, name, gameid, numbers) VALUES (?, ?, ?, ?)" (sesid, name, gameid, "00000")
-    execute conn "UPDATE game SET participants = participants + 1 WHERE gameid = ?" (gameid)
+initUser conn (User name gameId) = do
+    sesid <- stringRandomIO "................"
+    execute conn "INSERT INTO session (sesid, name, gameid, numbers) VALUES (?, ?, ?, ?)" (sesid :: Data.Text.Text, name :: String, gameId :: Int, "00000" :: String)
+    execute conn "UPDATE game SET participants = participants + 1 WHERE gameid = ?" (Only gameId)
     return sesid
 
-getFreeId :: Connection -> Int
+getFreeId :: Connection -> IO Int
 getFreeId conn = do
-    [[nextid :: Int]] <- query_ conn "SELECT gameid FROM game ORDER BY gameid DESC LIMIT 1"
-    return $ nextid + 1
-    --return ((unsafeLocalState nextid) + 1)
+    games <- query_ conn "SELECT gameid FROM game ORDER BY gameid DESC LIMIT 1" :: IO [GetGame]
+    case games of
+        [] -> return 1
+        [GetGame _ id] -> return (id + 1)
+        _ -> return 0
